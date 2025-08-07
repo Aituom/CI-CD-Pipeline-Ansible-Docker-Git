b1d435b349e11baf528e22d740fe78c31121ebe9
Aituom <96775000+Aituom@users.noreply.github.com>
2025-08-07 19:09:28 +0500

    Update mail.sh

--- notify/mail.sh
+++ notify/mail.sh
@@ -4,4 +4,5 @@ TO="temanomad@gmail.com"
 SUBJECT="Nginx deployed!"
 BODY="The latest version of Nginx was deployed successfully."
 
-echo "$BODY" | mail -s "$SUBJECT" "$TO"
+echo -e "Subject: $SUBJECT\n\n$BODY" | msmtp "$TO"
+
