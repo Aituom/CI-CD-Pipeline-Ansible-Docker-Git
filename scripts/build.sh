cad630494c962c4ff39910c203573aaeacf09412
Aituom <96775000+Aituom@users.noreply.github.com>
2025-08-07 18:15:23 +0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! docker image inspect myapp/nginx:latest > /dev/null 2>&1; then
-  docker build -t myapp/nginx:latest .
+  docker build -t myapp/nginx:latest -f ./scripts/Dockerfile ./scripts
 else
   echo "Docker image already exists. Skipping build."
 fi
